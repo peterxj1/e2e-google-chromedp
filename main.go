777653a5ed39ba964fcea848035392a943298c7c
peter <peter.johansson@zapote.se>
2022-10-03 23:42:04 +0200

    ok

--- main.go
+++ main.go
@@ -3,20 +3,35 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 	"time"
 
 	"github.com/chromedp/chromedp"
 )
 
+func createAllocatorContext(ctx context.Context, addr string) (context.Context, context.CancelFunc) {
+	if addr == "" {
+		opts := append(chromedp.DefaultExecAllocatorOptions[:],
+			chromedp.Flag("headless", false),
+			chromedp.Flag("disable-gpu", false),
+			chromedp.Flag("enable-automation", false),
+			chromedp.Flag("disable-extensions", false),
+		)
+		return chromedp.NewExecAllocator(ctx, opts...)
+	}
+
+	return chromedp.NewRemoteAllocator(ctx, addr)
+}
+
 func main() {
-	var username, password string
+
+	var chromeUrl, username, password string
 	flag.StringVar(&username, "username", "test@test", "login username")
 	flag.StringVar(&password, "password", "password", "login password")
+	flag.StringVar(&chromeUrl, "chromeUrl", "ws://localhost:9222", "chrome url")
 	flag.Parse()
 
-	actx, acancel := chromedp.NewRemoteAllocator(context.Background(), "ws://localhost:9222")
+	actx, acancel := createAllocatorContext(context.Background(), chromeUrl)
 	defer acancel()
 
 	ctx, cancel := chromedp.NewContext(
@@ -28,24 +43,21 @@ func main() {
 	ctx, cancel = context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 
-	stuff := ""
+	got := ""
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(`https://test.apply.se/soeb`),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			fmt.Println("1")
-			return nil
-		}),
 		chromedp.SendKeys("//input[@type='email']", username, chromedp.BySearch),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			fmt.Println("2")
-			return nil
-		}),
 		chromedp.SendKeys("//input[@type='password']", password, chromedp.BySearch),
 		chromedp.Click("//button[@type='submit']", chromedp.BySearch),
-		chromedp.Value(`//p[contains(@class, 'text-error')]`, &stuff),
+		chromedp.Text(`//p[contains(@class, 'text-error')]`, &got),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("stuff:\n%s", stuff)
+
+	want := "Felaktigt användarnamn eller lösenord"
+	if got != want {
+		log.Fatalf("expected %s got %s\n", want, got)
+	}
+
 }
